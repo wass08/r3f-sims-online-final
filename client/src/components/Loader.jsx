1e66773db499aa4a36b9249b6d8f5830250d8efb
wass08 <wass08@gmail.com>
2023-09-20 07:49:36 +0900

    loader disappear

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -9,7 +9,7 @@ export const Loader = ({ loaded }) => {
     <div className="fixed w-screen h-screen top-0 left-0 grid place-items-center pointer-events-none select-none">
       <div
         className={`text-center transition-opacity duration-1000 ${
-          progress < 100 && !loaded ? "opacity-0" : ""
+          progress === 100 || loaded ? "opacity-0" : ""
         }`}
       >
         <h1 className="font-bold text-5xl text-black">WAWA MANSION</h1>
